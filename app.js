64d1f172532e7522ca32a2e2269d4077ba50f262
HarshChheda <harshkc0132@gmail.com>
2019-04-08 22:29:09 +0530

    Added the table commands and Queries

--- app.js
+++ app.js
@@ -14,11 +14,75 @@ const con = mysql.createConnection({
 });
 
 function create_tables(con) {
-    var sql =
-    ``;
-    con.query(sql, function (err, result) {
+    var animal_species_table_sql =
+    `CREATE TABLE IF NOT EXISTS ${constants.animal_species_table} 
+    ( animal_species VARCHAR(255) NOT NULL PRIMARY KEY,
+    common_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    _genus VARCHAR(255),
+    _order VARCHAR(255),
+    _class VARCHAR(255),
+    _phylum VARCHAR(255),
+    _status VARCHAR(100)
+    );`;
+    con.query(animal_species_table_sql, function (err, result) {
         if (err) throw err;
-        console.log("Species Table created");
+        console.log(" Animal Species Table created");
+    });
+
+    var animal_table_sql =
+    `CREATE TABLE IF NOT EXISTS ${constants.animals_table} 
+    ( animal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+        name VARCHAR(255), 
+        dob DATE, 
+        status VARCHAR(100),
+        gender VARCHAR(100),
+        description TEXT,
+        photo BLOB, 
+        animal_species VARCHAR(255) NOT NULL REFERENCES animal_species(animal_species),
+        location POINT);`;
+    con.query(animal_table_sql, function (err, result) {
+        if (err) throw err;
+        console.log(" Animal Table created");
+    });
+
+    var plant_species_table_sql =
+    `CREATE TABLE IF NOT EXISTS ${constants.plant_species_table} 
+    ( plant_species VARCHAR(255) NOT NULL PRIMARY KEY,
+    common_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    _genus VARCHAR(255),
+    _order VARCHAR(255),
+    _class VARCHAR(255),
+    _phylum VARCHAR(255),
+    _status VARCHAR(100)
+    );`;
+    con.query(plant_species_table_sql, function (err, result) {
+        if (err) throw err;
+        console.log(" Plant Species Table created");
+    });
+
+    var plants_table_sql =
+    `CREATE TABLE IF NOT EXISTS ${constants.plants_table} 
+    ( plant_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+        description TEXT,
+        number INT, 
+        status VARCHAR(100),
+        photo BLOB, 
+        plant_species VARCHAR(255) REFERENCES plant_species(plant_species),
+        location POINT);`;
+    con.query(plants_table_sql, function (err, result) {
+        if (err) throw err;
+        console.log("Plants Table created");
+    });
+
+    var area_table_sql =
+    `CREATE TABLE IF NOT EXISTS ${constants.area_table} 
+    ( name VARCHAR(255) NOT NULL PRIMARY KEY,
+    boundary POLYGON);`;
+    con.query(area_table_sql, function (err, result) {
+        if (err) throw err;
+        console.log("Area Table created");
     });
 }
 con.connect(function (err) {
